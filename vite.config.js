dc919be2f51f53430037d2f990b40fe20fc8a931
Antonio Tapia <antoniotapiaalvarez@gmail.com>
2026-07-06 17:28:21 -0500

    Declarcion multiples HTML al build Vite

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,18 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         success: resolve(__dirname, 'success.html'),
-        cancel: resolve(__dirname, 'cancel.html')
+        cancel: resolve(__dirname, 'cancel.html'),
+      
+        auth: resolve(__dirname, 'auth.html'),
+        perfil: resolve(__dirname, 'perfil.html'),
+        historial: resolve(__dirname, 'historial.html'),
+
+        privacidad: resolve(__dirname, 'privacidad.html'),
+        terminos: resolve(__dirname, 'terminos.html'),
+        cookies: resolve(__dirname, 'cookies.html'),
+        devoluciones: resolve(__dirname, 'devoluciones.html'),
+        proteccionDatos: resolve(__dirname, 'proteccion-datos.html')
+        
       }
     }
   }
